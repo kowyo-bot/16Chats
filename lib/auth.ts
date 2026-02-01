a242b2c35ce49512858b18c25bc1fcf0ef04f7cf
Kowyo <kowyo@outlook.com>
2026-02-02 01:56:10 +0800

    feat: init auth

---
+++ lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import Database from "better-sqlite3";
+
+export const auth = betterAuth({
+  database: new Database("./sqlite.db"),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // 5 minutes
+    },
+  },
+});
+
+export type Session = typeof auth.$Infer.Session;
